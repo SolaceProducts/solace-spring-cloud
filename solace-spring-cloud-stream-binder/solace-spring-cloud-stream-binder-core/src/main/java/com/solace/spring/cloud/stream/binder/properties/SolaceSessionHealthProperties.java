8772842e9ed6c4d29f061e8bd18cdbaf11ab1b26
Jeffrey Douangpaseuth <11084623+Nephery@users.noreply.github.com>
2023-10-23 16:22:54 -0400

    add docs for new session health options

--- solace-spring-cloud-stream-binder/solace-spring-cloud-stream-binder-core/src/main/java/com/solace/spring/cloud/stream/binder/properties/SolaceSessionHealthProperties.java
+++ solace-spring-cloud-stream-binder/solace-spring-cloud-stream-binder-core/src/main/java/com/solace/spring/cloud/stream/binder/properties/SolaceSessionHealthProperties.java
@@ -9,6 +9,13 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @ConfigurationProperties("solace.health-check.connection")
 public class SolaceSessionHealthProperties {
+	/**
+	 * <p>The number of session reconnect attempts until the health goes {@code DOWN}. This will happen regardless if
+	 * the underlying session is actually still reconnecting. Setting this to {@code 0} will disable this feature.</p>
+	 * <p>This feature operates independently of the PubSub+ session reconnect feature. Meaning that if PubSub+
+	 * session reconnect is configured to retry less than the value given to this property, then this feature
+	 * effectively does nothing.</p>
+	 */
 	@Min(0)
 	@Getter
 	@Setter
