0b3d67d33b05111a5a923762f578a8c666dc3679
Sebastian Kirsch <sebastiankirsch@users.noreply.github.com>
2026-07-10 16:14:45 +0200

    Change condition for SolaceMeterConfiguration from MeterRegistry class to bean
    Fixes #497

--- solace-spring-cloud-stream-binder/solace-spring-cloud-stream-binder/src/main/java/com/solace/spring/cloud/stream/binder/config/SolaceMeterConfiguration.java
+++ solace-spring-cloud-stream-binder/solace-spring-cloud-stream-binder/src/main/java/com/solace/spring/cloud/stream/binder/config/SolaceMeterConfiguration.java
@@ -3,12 +3,12 @@ package com.solace.spring.cloud.stream.binder.config;
 import com.solace.spring.cloud.stream.binder.meter.SolaceMessageMeterBinder;
 import com.solace.spring.cloud.stream.binder.meter.SolaceMeterAccessor;
 import io.micrometer.core.instrument.MeterRegistry;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
-@ConditionalOnClass(MeterRegistry.class)
+@ConditionalOnBean(MeterRegistry.class)
 public final class SolaceMeterConfiguration {
 	@Bean
 	SolaceMessageMeterBinder solaceMessageMeterBinder() {
